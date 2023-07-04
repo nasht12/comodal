271105f88af98750f3b9749215efdeaea09cc816
abhinash tummala <abhinash.t12@gmail.com>
2023-07-04 13:29:58 -0700

    added tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import Image from 'next/image'
-import Menu from '../../components/Menu/Menu'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Comodal
-      <Menu />
-    </main>
+    <>
+      {/* Comodal */}
+    </>
   )
 }
